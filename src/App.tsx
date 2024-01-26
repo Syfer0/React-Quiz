3d5a6bd8cec0fffa63d00226da3ff2ce8912e9e3
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-26 22:58:14 +0530

    Error handing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import DateCounter from "./DateCounter";
 import Header from "./Header";
+
+const initialState = {
+  question: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Unknown action ");
+  }
+}
 function App() {
-  useEffect(function () {}, []);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:5000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
