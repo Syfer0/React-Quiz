1cceca3a637bfe2ed27bb1b78cf52a3188431def
Arman <125900320+Syfer0@users.noreply.github.com>
2024-02-20 12:37:26 +0530

    Error src/App.tsx  hot updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Header from "./Header";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { useEffect, useReducer } from "react";
 import Main from "./Min";
